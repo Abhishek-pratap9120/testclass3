f4f6789cff416cb8199a5308de5567b0441bb046
Ayush Agrawal <ayushagrawalspr12345@gmail.com>
2025-08-01 13:42:02 +0530

    V3

--- main.tf
+++ main.tf
@@ -24,3 +24,7 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
+resource "azurerm_resource_group" "example3" {
+  name     = "example-resoudsdhosrces"
+  location = "West Europe"
+}
